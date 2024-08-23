693753e742b7afa7097b36f21e9b879f60154af1
hydroshiba <phanhaiminh.hcm@gmail.com>
2024-08-23 14:13:16 +0700

    Update: Reorganize header files

---
+++ dst.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file dst.hpp
+ * @brief Wrapper header file for the dynamic segment tree and its wrapper structures.
+ */
+
+#ifndef DST_HPP_
+#define DST_HPP_
+
+#include "dst/tree.hpp"
+#include "dst/aggregate_set.hpp"
+
+#endif
